72b376c8f5efc40a0c3d6c47b500bd972c0093aa
Salome Shion <brocoli1992@hotmail.co.jp>
2022-01-02 23:58:40 +0900

    プロジェクト ファイルを追加します。

---
+++ RSA_num2alpha_py/RSA_num2alpha_py.py
@@ -0,0 +1,28 @@
+# 数値→アルファベット
+import re
+def num2alpha(M):
+    if len(str(M)) % 2 != 0:
+        msg = ("数値：" + str(M) + "の値が偶数桁でなかったので、この関数を動かせません")
+        return msg
+    else:
+        words = ""
+        list = re.split('(..)',str(M))[1::2]
+        for i in range(len(list)):
+            words += chr(64+int(list[i]))
+        return words
+
+p=37 
+q=71 
+e=79
+c=904
+n=p*q 
+print("n=:",n) 
+for m in range(e): 
+    if m*(p-1)*(q-1) % e == e-1: 
+      print("m=:",m) 
+      break 
+d=(m*(p-1)*(q-1)+1)//e
+print("d=:",d)
+M=(c**d)%n
+print("M=:",M)
+print("出力結果:",num2alpha(M))
